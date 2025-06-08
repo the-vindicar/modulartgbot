2cf366f42393b447cb8a2cf4c0e5efa94193a55a
Vindicar <the.vindicar@gmail.com>
2025-06-08 22:49:44 +0300

    Fixed task error logging for cancelled tasks.

--- api/_tools.py
+++ api/_tools.py
@@ -8,21 +8,25 @@ __all__ = ['background_task']
 
 
 def done_callback(task: asyncio.Task):
-    exc = task.exception()
-    if exc is None or isinstance(exc, asyncio.CancelledError):
-        return
-    exc_info = (type(exc), exc, exc.__traceback__)
-    task_name = task.get_name()
-    coro_name = task.get_coro().__qualname__
-    logging.root.critical('Uncaught exception in task %s running %s():',
-                          task_name, coro_name, exc_info=exc_info)
+    try:
+        exc = task.exception()
+    except asyncio.CancelledError:
+        pass
+    else:
+        if exc is None or isinstance(exc, asyncio.CancelledError):
+            return
+        exc_info = (type(exc), exc, exc.__traceback__)
+        task_name = task.get_name()
+        coro_name = task.get_coro().__qualname__
+        logging.root.critical('Uncaught exception in task %s running %s():',
+                              task_name, coro_name, exc_info=exc_info)
 
 
 @contextlib.asynccontextmanager
 async def background_task(coro: t.Coroutine):
     """Гарантирует фоновый запуск указанной корутины при входе в асинхронный контекст (async with),
     и её завершение (отмену) при выходе из этого контекста."""
-    task = asyncio.create_task(coro)
+    task = asyncio.create_task(coro, name=f'{coro.__qualname__}()@{coro.cr_code.co_filename}')
     task.add_done_callback(done_callback)
     try:
         yield task
