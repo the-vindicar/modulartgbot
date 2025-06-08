560c6ce8a668b11b911f0de286d832ebf2e37911
Vindicar <the.vindicar@gmail.com>
2025-06-08 22:22:21 +0300

    Now we're able to see exceptions that happen in background tasks.

--- api/_tools.py
+++ api/_tools.py
@@ -1,9 +1,21 @@
 import asyncio
 import contextlib
+import logging
 import typing as t
 
 
 __all__ = ['background_task']
+
+
+def done_callback(task: asyncio.Task):
+    exc = task.exception()
+    if exc is None or isinstance(exc, asyncio.CancelledError):
+        return
+    exc_info = (type(exc), exc, exc.__traceback__)
+    task_name = task.get_name()
+    coro_name = task.get_coro().__qualname__
+    logging.root.critical('Uncaught exception in task %s running %s():',
+                          task_name, coro_name, exc_info=exc_info)
 
 
 @contextlib.asynccontextmanager
@@ -11,6 +23,7 @@ async def background_task(coro: t.Coroutine):
     """Гарантирует фоновый запуск указанной корутины при входе в асинхронный контекст (async with),
     и её завершение (отмену) при выходе из этого контекста."""
     task = asyncio.create_task(coro)
+    task.add_done_callback(done_callback)
     try:
         yield task
     finally:
