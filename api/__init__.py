adf5e11b966cb64802e5bb32149eb3d511793fed
Vindicar <the.vindicar@gmail.com>
2025-10-02 19:33:55 +0300

    Typo -_-

--- api/__init__.py
+++ api/__init__.py
@@ -5,4 +5,4 @@ from ._loader import modules_lifespan
 from ._tools import *
 from ._config_manager import ConfigManagerImpl
 from ._logs import setup_logging
-from _quart_hax import *
+from ._quart_hax import *
